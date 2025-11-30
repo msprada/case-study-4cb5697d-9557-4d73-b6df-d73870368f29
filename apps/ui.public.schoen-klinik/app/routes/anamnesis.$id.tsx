d35af747b1485d3df54bad52d2128996ecaed7dd
Marcel Sprada <msprada@web.de>
2025-11-30 13:38:00 +0100

    - implment insert form with dummy api endpoint

--- apps/ui.public.schoen-klinik/app/routes/anamnesis.$id.tsx
+++ apps/ui.public.schoen-klinik/app/routes/anamnesis.$id.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/anamnesis.$id";
-import { Form } from "react-router";
+import { data, Form } from "react-router";
 import prisma from "../utils/prisma.server";
 
 export function meta({ }: Route.MetaArgs) {
@@ -17,8 +17,8 @@ export async function loader({ params }: Route.LoaderArgs) {
     return { tempLink: null }
   }
   const tempLink = await prisma.tempLink.findFirst({
-    where:{
-      id: {equals: id}
+    where: {
+      id: { equals: id }
     }
   })
 
@@ -28,9 +28,39 @@ export async function loader({ params }: Route.LoaderArgs) {
 export async function action({
   request,
 }: Route.ActionArgs) {
+
+
+
   let formData = await request.formData();
   let title = formData.get("title");
-  return { title: title ? title.toString() : "fallback" };
+
+
+  const apiUrl = `${process.env.API_URL}/anamnesis`;
+
+  try {
+    if (!process.env.API_RESSOURCE_ANAMNESIS_URL) {
+      throw new Error("API_RESSOURCE_ANAMNESIS_URL is not defined");
+    }
+
+    const res = await fetch(apiUrl, {
+      headers: {
+        Authorization: `Bearer ${process.env.API_TOKEN}`,
+      },
+      method: "POST",
+      body: JSON.stringify({ title }),
+    });
+
+    const data = await res.json();
+
+    return { title: title ? title.toString() : "fallback" };
+  }
+
+  catch (error) {
+    console.error("Error creating anamnesis:", error);
+    return data({ error }, { status: 400 })
+  }
+
+
 }
 
 export default function Anamnesis({
@@ -39,16 +69,22 @@ export default function Anamnesis({
 
   const { tempLink } = loaderData;
 
+  let title;
+
+  if (actionData && typeof actionData === "object" && 'title' in actionData) {
+    title = actionData.title;
+  }
+
   return (
 
-    (tempLink && typeof tempLink ==='object' && 'id' in tempLink && tempLink.id) ? (<div>
+    (tempLink && typeof tempLink === 'object' && 'id' in tempLink && tempLink.id) ? (<div>
       <h1>Anamnese Bogen</h1>
       <Form method="post">
         <input type="text" name="title" />
         <button type="submit">Submit</button>
       </Form>
-      {actionData ? (
-        <p>{actionData.title} updated</p>
+      {title ? (
+        <p>{title} updated</p>
       ) : null}
     </div>) : <p>You are not allowed to see the document!</p>
 
