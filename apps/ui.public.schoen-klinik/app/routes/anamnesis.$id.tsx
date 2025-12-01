bb28c3e04be764e9182c2a5485d41b965cc161a1
Marcel Sprada <msprada@web.de>
2025-12-01 17:09:55 +0100

    - give feedback to the user

--- apps/ui.public.schoen-klinik/app/routes/anamnesis.$id.tsx
+++ apps/ui.public.schoen-klinik/app/routes/anamnesis.$id.tsx
@@ -36,7 +36,7 @@ export async function action({
   const address = formData.get("address");
   const mainMedicalDisorder = formData.get("mainMedicalDisorder");
   const furtherMedicalDisorder = formData.get("furtherMedicalDisorder");
-  const dummyMail=formData.get("email")
+  const dummyMail = formData.get("email")
 
   if (!firstname) {
     errors.firstname = "Bitte geben Sie ein ihren Vornamen an.";
@@ -114,69 +114,77 @@ export default function Anamnesis({
 
     (tempLink && typeof tempLink === 'object' && 'id' in tempLink && tempLink.id) ? (<div>
 
-      <div className="flex flex-col mb-4">
-        <h1>Anamnese Bogen</h1>
-        <p>Bitte füllen Sie Anamnesebogen aus und best&auml;tigen Sie im Anschluss die Schaltfläche.</p>
-      </div>
-      <Form method="post">
-        <legend className="mb-4"><i>Registrierungsformular</i></legend>
-        <fieldset>
-          <div className="flex flex-row justify-end gap-2 mb-4">
-            <label htmlFor="firstname">
-              Vorname:
-            </label>
-            <input className="border-2 boder-solid" aria-required type="text" name="firstname" id="firstname" />
-          </div>
-          <div className="flex flex-row justify-end gap-2 mb-4">
-            <span><strong>{errors?.firstname ? <p style={{ color: "red" }}>{errors.firstname}</p> : null}</strong></span>
-          </div>
+      {!submitted ? (
 
-          <div className="flex flex-row justify-end gap-2 mb-4">
-            <label htmlFor="lastname">
-              Nachname:
-            </label>
-            <input className="border-2 boder-solid" aria-required type="text" name="lastname" id="lastname" />
+        <>
+          <div className="flex flex-col mb-4">
+            <h1>Anamnese Bogen</h1>
+            <p>Bitte füllen Sie Anamnesebogen aus und best&auml;tigen Sie im Anschluss die Schaltfläche.</p>
           </div>
-          <div className="flex flex-row justify-end gap-2 mb-4">
-            <span><strong>{errors?.lastname ? <p style={{ color: "red" }}>{errors.lastname}</p> : null}</strong></span>
-          </div>
+          <Form method="post">
+            <legend className="mb-4"><i>Registrierungsformular</i></legend>
+            <fieldset>
+              <div className="flex flex-row justify-end gap-2 mb-4">
+                <label htmlFor="firstname">
+                  Vorname:
+                </label>
+                <input className="border-2 boder-solid" aria-required type="text" name="firstname" id="firstname" />
+              </div>
+              <div className="flex flex-row justify-end gap-2 mb-4">
+                <span><strong>{errors?.firstname ? <p style={{ color: "red" }}>{errors.firstname}</p> : null}</strong></span>
+              </div>
 
-          <div className="flex flex-row justify-end gap-2 mb-4">
-            <label htmlFor="address">
-              Adresse:
-            </label>
-            <input className="border-2 boder-solid" aria-required type="text" name="address" id="address" />
-          </div>
-          <div className="flex flex-row justify-end gap-2 mb-4">
-            <span><strong>{errors?.address ? <p style={{ color: "red" }}>{errors.address}</p> : null}</strong></span>
-          </div>
+              <div className="flex flex-row justify-end gap-2 mb-4">
+                <label htmlFor="lastname">
+                  Nachname:
+                </label>
+                <input className="border-2 boder-solid" aria-required type="text" name="lastname" id="lastname" />
+              </div>
+              <div className="flex flex-row justify-end gap-2 mb-4">
+                <span><strong>{errors?.lastname ? <p style={{ color: "red" }}>{errors.lastname}</p> : null}</strong></span>
+              </div>
 
-          <div className="flex flex-row justify-end gap-2 mb-4">
-            <label htmlFor="mainMedicalDisorder">
-              Hauptbeschwerde:
-            </label>
-            <input className="border-2 boder-solid" aria-required type="text" name="mainMedicalDisorder" id="mainMedicalDisorder" />
-          </div>
-          <div className="flex flex-row justify-end gap-2 mb-4">
-            <span><strong>{errors?.mainMedicalDisorder ? <p style={{ color: "red" }}>{errors.mainMedicalDisorder}</p> : null}</strong></span>
-          </div>
+              <div className="flex flex-row justify-end gap-2 mb-4">
+                <label htmlFor="address">
+                  Adresse:
+                </label>
+                <input className="border-2 boder-solid" aria-required type="text" name="address" id="address" />
+              </div>
+              <div className="flex flex-row justify-end gap-2 mb-4">
+                <span><strong>{errors?.address ? <p style={{ color: "red" }}>{errors.address}</p> : null}</strong></span>
+              </div>
 
-          <div className="flex flex-row justify-end gap-2 mb-4">
-            <label htmlFor="furtherMedicalDisorder">
-              Weitere Beschwerden:
-            </label>
-            <input className="border-2 boder-solid" type="text" name="furtherMedicalDisorder" id="furtherMedicalDisorder" />
-          </div>
+              <div className="flex flex-row justify-end gap-2 mb-4">
+                <label htmlFor="mainMedicalDisorder">
+                  Hauptbeschwerde:
+                </label>
+                <input className="border-2 boder-solid" aria-required type="text" name="mainMedicalDisorder" id="mainMedicalDisorder" />
+              </div>
+              <div className="flex flex-row justify-end gap-2 mb-4">
+                <span><strong>{errors?.mainMedicalDisorder ? <p style={{ color: "red" }}>{errors.mainMedicalDisorder}</p> : null}</strong></span>
+              </div>
 
-        </fieldset>
-        <div className="flex flex-row justify-end gap-2 mb-4">
-          <button className="btn text-white bg-orange-500 rounded-full w-64" type="submit">Absenden</button>
-        </div>
-        <input type="hidden" value="dummy@mail.de" name="email"></input>
-      </Form>
-      {submitted ? (
-        <p>wurde erfolgreich versendet!</p>
-      ) : null}
+              <div className="flex flex-row justify-end gap-2 mb-4">
+                <label htmlFor="furtherMedicalDisorder">
+                  Weitere Beschwerden:
+                </label>
+                <input className="border-2 boder-solid" type="text" name="furtherMedicalDisorder" id="furtherMedicalDisorder" />
+              </div>
+
+            </fieldset>
+            <div className="flex flex-row justify-end gap-2 mb-4">
+              <button className="btn text-white bg-orange-500 rounded-full w-64" type="submit">Absenden</button>
+            </div>
+            <input type="hidden" value="dummy@mail.de" name="email"></input>
+          </Form>
+        </>
+      ) :  <div className="flex flex-col mb-4">
+            <h1>Anamnese Bogen</h1>
+            <p>Der Bogen wurde erfolgreich versendet. Bitte warten Sie auf eine Rückmeldung unsererseits.</p>
+          </div>}
+
+
+
     </div>) : <p>Sie haben keine gültige Anmeldung und können den Anmeldebogen nicht aufrufen. Bitte erstellen Sie in Login. <Link to={{ pathname: "/sign-in", }}>hier geht es zu Anmeldung</Link></p>
 
   )
