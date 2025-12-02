d6ebbe7af3a5553924c61335b28b3ea939c850b0
= <=>
2025-12-02 23:15:19 +0100

    - run prettier on ui.public

--- apps/ui.public.schoen-klinik/app/prisma/prisma.config.ts
+++ apps/ui.public.schoen-klinik/app/prisma/prisma.config.ts
@@ -1,15 +1,15 @@
-import 'dotenv/config'
-import path from "node:path";
-import { defineConfig, type PrismaConfig, env } from "prisma/config";
+import 'dotenv/config';
+import path from 'node:path';
+import { defineConfig, type PrismaConfig, env } from 'prisma/config';
 
 const prismaConfig = {
-  schema: path.join("schema.prisma"),
+  schema: path.join('schema.prisma'),
   migrations: {
-    path: "prisma/migrations",
+    path: 'prisma/migrations',
   },
   // engine: "classic",
   datasource: {
-    url: env("DATABASE_URL")
+    url: env('DATABASE_URL'),
   },
 } satisfies PrismaConfig;
 
