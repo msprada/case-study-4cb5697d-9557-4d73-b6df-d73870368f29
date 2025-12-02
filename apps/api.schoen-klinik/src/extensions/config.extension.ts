558a475d81e159dd2324f36d3be6ff034d86b3b1
= <=>
2025-12-02 23:18:48 +0100

    - run prettier

--- apps/api.schoen-klinik/src/extensions/config.extension.ts
+++ apps/api.schoen-klinik/src/extensions/config.extension.ts
@@ -1,33 +1,33 @@
-import fastifyEnv from "@fastify/env";
-import { type FastifyInstance } from "fastify";
-import type { PrismaClient } from "@prisma/client/extension";
+import fastifyEnv from '@fastify/env';
+import { type FastifyInstance } from 'fastify';
+import type { PrismaClient } from '@prisma/client/extension';
 
 const schema = {
-    type: "object",
-    required: ["KEY", "DATABASE_URL"],
-    properties: {
-        KEY: { type: "string", default: "" },
-        DATABASE_URL: { type: "string", default: "file:./data/schon-klinik.db" }
-    },
+  type: 'object',
+  required: ['KEY', 'DATABASE_URL'],
+  properties: {
+    KEY: { type: 'string', default: '' },
+    DATABASE_URL: { type: 'string', default: 'file:./data/schon-klinik.db' },
+  },
 } as const;
 
 const options = {
-    schema,
-    dotenv: true,
-    data: process.env,
+  schema,
+  dotenv: true,
+  data: process.env,
 };
 
-declare module "fastify" {
-    interface FastifyInstance {
-        config: {
-            KEY: string;
-            DATABASE_URL:string
-        },
-        prisma: PrismaClient
-    }
-};
+declare module 'fastify' {
+  interface FastifyInstance {
+    config: {
+      KEY: string;
+      DATABASE_URL: string;
+    };
+    prisma: PrismaClient;
+  }
+}
 
 export default async function configExtension(fastify: FastifyInstance) {
-    await fastify.register(fastifyEnv, options);
-    return fastify
+  await fastify.register(fastifyEnv, options);
+  return fastify;
 }
--- apps/api.schoen-klinik/src/routes/anamnesis.ts
+++ apps/api.schoen-klinik/src/routes/anamnesis.ts
@@ -1,45 +1,63 @@
-import { type FastifyInstance, type FastifyPluginOptions, type FastifyReply, type FastifyRequest } from "fastify";
-import { type FromSchema } from "json-schema-to-ts";
-import {anamnesisDocumentBodyJsonSchema, anamnesisDocumentJsonSchema} from "../schema/anamnesis/anamnesis-document-create.js";
+import {
+  type FastifyInstance,
+  type FastifyPluginOptions,
+  type FastifyReply,
+  type FastifyRequest,
+} from 'fastify';
+import { type FromSchema } from 'json-schema-to-ts';
+import {
+  anamnesisDocumentBodyJsonSchema,
+  anamnesisDocumentJsonSchema,
+} from '../schema/anamnesis/anamnesis-document-create.js';
 
 async function anamnesisRoutes(fastify: FastifyInstance, options: FastifyPluginOptions) {
+  fastify.post<{ Body: FromSchema<typeof anamnesisDocumentBodyJsonSchema> }>(
+    '/',
+    {
+      schema: {
+        body: anamnesisDocumentBodyJsonSchema,
+        response: {
+          201: anamnesisDocumentJsonSchema,
+        } as const,
+      },
+    },
+    async (request, reply) => {
+      try {
+        const {
+          firstname,
+          lastname,
+          address,
+          email,
+          mainMedicalDisorder,
+          furtherMedicalDisorder,
+          notes,
+        } = request.body;
 
+        console.log({ body: request.body });
 
-    fastify.post<{ Body: FromSchema<typeof anamnesisDocumentBodyJsonSchema> }>('/', {
-        schema: {
-            body: anamnesisDocumentBodyJsonSchema,
-            response: {
-                201: anamnesisDocumentJsonSchema
-            } as const
+        if (!email) {
+          reply.code(400).send({ error: 'Email is required' });
+          return;
         }
-    }, async (request, reply) => {
-        try {
 
-            const {firstname, lastname, address, email, mainMedicalDisorder, furtherMedicalDisorder,notes} =request.body
+        const newAnamnesisDocument = await fastify.prisma.anamnesisDocument.create({
+          data: {
+            mainMedicalDisorder: mainMedicalDisorder,
+            furtherMedicalDisorder: furtherMedicalDisorder,
+            notes: notes,
+            email: email,
+          },
+        });
+        reply.code(201).send(newAnamnesisDocument);
+      } catch (error) {
+        if (error instanceof Error) {
+          request.log.error(`Error creating anamnesisDocument: ${error.message}`);
+        }
 
-            console.log({body: request.body});
-
-            if(!email){
-                reply.code(400).send({ error: 'Email is required' });
-                return;
-            }
-
-            const newAnamnesisDocument = await fastify.prisma.anamnesisDocument.create({
-                data: {
-                    mainMedicalDisorder:mainMedicalDisorder,
-                    furtherMedicalDisorder:furtherMedicalDisorder,
-                    notes:notes,
-                    email:email
-                }
-            });
-            reply.code(201).send(newAnamnesisDocument);
-        } catch (error) {
-            if (error instanceof Error) {
-                request.log.error(`Error creating anamnesisDocument: ${error.message}`);
-            }
-
-            reply.code(500).send({ error: 'Failed to create anamnesisDocument' });
-        }});
+        reply.code(500).send({ error: 'Failed to create anamnesisDocument' });
+      }
+    },
+  );
 }
 
 export default anamnesisRoutes;
